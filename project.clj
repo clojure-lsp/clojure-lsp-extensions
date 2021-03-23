a7cf96b721d77bc52dc653345a7c65182f54f2f2
Eric Dallo <ericdallo06@hotmail.com>
2021-03-22 22:52:04 -0300

    Fix java version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-lsp/clojure-lsp-extensions "1.0.0"
+(defproject clojure-lsp/clojure-lsp-extensions "1.0.1"
   :description "clojure-lsp java extensions"
   :url "http://github.com/clojure-lsp/clojure-lsp-extensions"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,4 +6,5 @@
   :java-source-paths ["src-java"]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.eclipse.lsp4j/org.eclipse.lsp4j  "0.11.0" :exclusions [org.eclipse.xtend/org.eclipse.xtend.lib]]]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :profiles {:uberjar {:aot :all}})
